03c7e3638bddae53602db3876a201ade05534045
Danielle Miike <danimiike@gmail.com>
2022-09-14 10:02:01 -0400

    Add files via upload

---
+++ Server Side/CustomerDAO.cs
@@ -0,0 +1,26 @@
+﻿using Casestudy.DAL.DomainClasses;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Casestudy.DAL.DAO
+{
+    public class CustomerDAO
+    {
+        private AppDbContext _db;
+        public CustomerDAO(AppDbContext ctx)
+        {
+            _db = ctx;
+        }
+        public async Task<Customer> Register(Customer customer)
+        {
+            await _db.Customers.AddAsync(customer);
+            await _db.SaveChangesAsync();
+            return customer;
+        }
+        public async Task<Customer> GetByEmail(string email)
+        {
+            Customer customer = await _db.Customers.FirstOrDefaultAsync(u => u.Email == email);
+            return customer;
+        }
+    }
+}
---
+++ Server Side/OrderController.cs
@@ -0,0 +1,61 @@
+﻿using Casestudy.DAL;
+using Casestudy.DAL.DAO;
+using Casestudy.DAL.DomainClasses;
+using Casestudy.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Casestudy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        AppDbContext _ctx;
+        public OrderController(AppDbContext context) // injected here
+        {
+            _ctx = context;
+        }
+        [HttpPost]
+        [Produces("application/json")]
+        public async Task<ActionResult<string>> Index(OrderHelper helper)
+        {
+            bool isbackorder = false;
+            string retVal = "";
+            try
+            {
+                CustomerDAO uDao = new CustomerDAO(_ctx);
+                Customer orderOwner = await uDao.GetByEmail(helper.email);
+                OrderDAO tDao = new OrderDAO(_ctx);
+                int orderId = await tDao.AddOrder(orderOwner.Id, helper.selections);
+
+                foreach (OrderSelectionHelper obj in helper.selections)
+                {
+                    if (obj.Qty > obj.item.QtyOnHand)
+                    {
+                        isbackorder = true;
+                        break;
+                    }
+                }
+
+                if (orderId > 0)
+                {
+                    retVal = "Order " + orderId + " created!";
+                    if (isbackorder)
+                        retVal += " Goods backordered";
+                }
+                else
+                {
+                    retVal = "Order not saved";
+                }
+            }
+            catch (Exception ex)
+            {
+                retVal = "Order not saved " + ex.Message;
+            }
+            return retVal;
+        }
+    }
+}
+
